5f54d3d0889e8ee5ba63d87c5b73ee471cd58c43
derek <derekle.creative@gmail.com>
2021-08-11 21:36:13 -0500

    commit tree a0237fb7e5b067654169de6f23cae002a2b886ae

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(a, s){
+    const results = a.filter(driver => driver.toLowerCase() == s.toLowerCase());
+    return results;
+}
+
+function fuzzyMatch(a, s){
+    const results = a.filter(driver => driver.charAt(0).toLowerCase() == s.charAt(0).toLowerCase());
+    return results;
+}
+
+function matchName(a, s){
+    const results = a.filter(driver => driver.name == s);
+    return results;
+}
